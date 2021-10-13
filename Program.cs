5a03ac206937da826fab335ddf960d5c94d0ef45
margaritakaiki@outlook.com <margaritakaiki@outlook.com>
2021-10-13 21:17:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<User>(); // Создание списка User
+            for (int i = 0; i < 2; i++) // Цикл для добавления
+            {
+                list.Add(Input());
+            }
+            Output(list);
+
+        }
+
+
+        /// <summary>
+        /// Метод для вывода
+        /// </summary>
+        /// <param name="list"></param>
+        private static void Output(List<User> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        /// <summary>
+        /// Метод для ввода данных.Возращает конструктор класса User.
+        /// </summary>
+        /// <returns></returns>
+        private static User Input()
+        {
+            string name, surname, middlename, mail;
+            int age;
+
+            Console.WriteLine("Введите имя:");
+            name = InputValidNull();
+            Console.WriteLine("Введите фамилию:");
+            surname = InputValidNull();
+            Console.WriteLine("Введите отчество:");
+            middlename = InputValidNull();
+            Console.WriteLine("Введите возраст:");
+            age = InputValidInt();
+            Console.WriteLine("Введите почту:");
+            mail = InputValidNull();
+            return new User(name, surname, middlename, age, mail);
+
+        }
+
+        /// <summary>
+        /// Метод проверки на целое число
+        /// </summary>
+        /// <returns></returns>
+        private static int InputValidInt()
+        {
+            string age;
+            while (true)
+            {
+                age = Console.ReadLine();
+                if (int.TryParse(age, out int ag))
+                {
+                    return ag;
+                }
+                else
+                {
+                    Console.WriteLine("Не верный формат. Повторите попытку.");
+                }
+            }         
+        }
+
+        /// <summary>
+        /// Метод проверки строки на пустоту (Null)
+        /// </summary>
+        /// <returns></returns>
+        private static string InputValidNull()
+        {
+            string text;
+            while (true)
+            {
+                text = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    Console.WriteLine("Данная строка не может быть пустой. Повторите попытку.");
+                }
+                else
+                {
+                    return text;
+                }
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string MiddleName { get; set; }
+        public int Age { get; set; }
+        public string Mail { get; set; }
+
+        /// <summary>
+        /// Коструктор с параметрами. 
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="middlename">Отчество</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="mail">Почта</param>
+        public User(string name, string surname, string middlename, int age, string mail)
+        {
+            Name = name;
+            SurName = surname;
+            MiddleName = middlename;
+            Age = age;
+            Mail = mail;
+        }
+
+        /// <summary>
+        /// Переопределение метода ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{Name} {SurName} {MiddleName} {Age} {Mail}";
+        }
+
+    }
+}
